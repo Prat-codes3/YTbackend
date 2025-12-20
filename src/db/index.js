97123e12db2736351b3eff609980a971d2c79081
Pratik <pratikwankhede503@gmail.com>
2025-12-20 12:49:29 +0530

    connected database

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+// import { DB_NAME } from "../constants.js";
+
+const connectDB= async ()=>{
+    try {
+        const uri=process.env.MONGODB_URI
+        if(!uri){console.log("not uri")}
+        const connectionInstance=await mongoose.connect(`${uri}`)
+        console.log(`\n MOngoBD connected !! DB HOST:${connectionInstance.connection.host}`)
+    } catch (error) {
+           console.error("ERROR",error)
+       // throw error or u can use proces.exit(1)
+           process.exit(1)
+    }
+}
+
+export default connectDB
---
+++ src/index.js
@@ -0,0 +1,32 @@
+// require('dotenv').config({path:'./env'}) or u cn do
+// console.log("CWD =", process.cwd());
+import dotenv from "dotenv"
+//   const result =dotenv.config({ path:'./env'}) //configure dotenv as soon as possible
+const result=dotenv.config({ path: "D:/YTBackend/.env" });  //.env didnt work so we gave full
+/*   console.log(result) 
+   console.log("HEloooooooo")
+ console.log("URI =", process.env.MONGODB_URI); we did this to check if contents of .env are being loaded */
+
+
+// import mongoose  from "mongoose";
+// import { DB_NAME } from "./constants";  we did this in aour seperate bd/index.js file
+import connectDB from "./db/index.js" 
+
+
+
+
+
+
+/*
+(async ()=>{
+    try {
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`) 
+    } catch (error) {
+        console.error("ERROR",error)
+        throw error
+    }
+})()                   u can connect database diresctly using IIFE  or u can make sepearte folders to store your code and then import from there ,we are doing importing in this project*/
+
+
+
+connectDB();
